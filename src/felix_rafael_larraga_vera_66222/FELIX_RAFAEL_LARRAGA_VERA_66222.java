d27dd71e1e75afde12536f0762c8c8bac917c452
FELIX LARRAGA <FELIX LARRAGA@192.168.1.70>
2021-02-25 20:15:52 -0600

    este mi primer repositorio.

---
+++ src/felix_rafael_larraga_vera_66222/FELIX_RAFAEL_LARRAGA_VERA_66222.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package felix_rafael_larraga_vera_66222;
+
+/**
+ *
+ * @author FELIX LARRAGA
+ */
+public class FELIX_RAFAEL_LARRAGA_VERA_66222 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      
+       int nombre ;
+       
+       
+        System.out.println("FELIX RAFAEL LARRAGA ");
+    }
+    
+}
